fb4e9e227312c5d6a4616a559fa4aca063999c45
Emily Patsko <emily.patsko@softwire.com>
2022-07-12 14:22:26 +0100

    Serve list of films and table of films

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+from re import L
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/films/list')
+def get_films_list():
+    return render_template('films_list.html')
+
+@app.route('/films/table')
+def get_films_table():
+    with open('films.csv', 'r') as f:
+        lines = f.read().splitlines()
+        lines = [line.split(',') for line in lines]
+        films = [{'title': line[0], 'rating': line[1]} for line in lines]
+    return render_template('films_table.html', films=films)
